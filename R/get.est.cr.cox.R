4d9c804c12352edf4d18028091ad09b9bde259a4
Marc Delord <mdelord@gmail.com>
2017-05-27 15:20:01 +0000

    version 2.4

--- R/get.est.cr.cox.R
+++ R/get.est.cr.cox.R
@@ -2,7 +2,7 @@
 get.est.cr.cox <-
 function( x , data , status , trans , cens.code , formula ){
   keep <- as.vector(sapply(strsplit( as.character(formula)[2] , '\\+' ) , function(x) gsub(" " , "", x) ))
-  x[x==0]<-1e-16
+  x[x==0]<-1e-7
   df1 <- data.frame( data , 'ns' = x )
   df2 <- mstate::crprep( data = df1 , Tstop = x , status = status , trans = trans , cens = cens.code , keep = keep )
   s1 <- with( df2 , survival::Surv( time = Tstart , time2 = Tstop , type = 'counting' , event = status==trans ) )
